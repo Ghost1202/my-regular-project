9d03bb7b104cb86b2655a4050c0915a7e294a20c
Ghost1202 <ehor760@gmail.com>
2026-04-15 18:10:49 +0000

    fix loggining

--- terraform/modules/logging/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_cloudwatch_log_group" "app" {
-  name              = "/${var.name}/app"
-  retention_in_days = 7
-}
-
-resource "aws_cloudwatch_log_group" "nginx" {
-  name              = "/${var.name}/nginx"
-  retention_in_days = 7
-}
