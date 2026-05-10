449328b2c5231bd9051029400449d7f58ca90340
Ghost1202 <ehor760@gmail.com>
2026-05-10 15:29:12 +0000

    fix code and add more check

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -54,9 +54,9 @@ resource "aws_lb_target_group" "this" {
     protocol            = "HTTP"
     matcher             = "200"
     interval            = 30
-    timeout             = 5
+    timeout             = 10
     healthy_threshold   = 2
-    unhealthy_threshold = 3
+    unhealthy_threshold = 5
   }
 
   tags = {
